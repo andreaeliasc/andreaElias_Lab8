be24cd8eccd9b0e57a64c5c45fc65ae74f44c986
DESKTOP-DMEFEJ6\Andrea <andrea.elias16@gmail.com>
2019-05-22 01:56:35 -0600

    juego

---
+++ lab8/Assets/Player.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+
+    public Text objectVisto;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray aim = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.5f));
+        RaycastHit hitInfo;
+        if (Physics.Raycast(aim, out hitInfo))
+        {
+            string newText = hitInfo.collider.tag;
+            if (newText == "Untagged" || newText == "Small1" || newText == "Small2" || newText == "Big")
+            {
+                objectVisto.text = "";
+            }
+            else
+            {
+                objectVisto.text = newText;
+            }
+        }
+       
+    }
+}
---
+++ lab8/Assets/linterna.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class linterna : MonoBehaviour
+{
+    bool flashlightState;
+    AudioSource audioSource;
+    public AudioClip flashlightStateSound;
+
+   
+    void Start()
+    {
+        flashlightState = true;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+           
+            flashlightState = !flashlightState;
+            gameObject.GetComponent<Light>().enabled = flashlightState;
+            if (audioSource != null && flashlightStateSound != null)
+            {
+                audioSource.PlayOneShot(flashlightStateSound);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+
+    }
+}
